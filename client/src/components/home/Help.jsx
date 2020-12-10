dfc89f4eb426336c6012235e727bf36ba6fdc99b
wahyu-k <35.wahyukurniawan@gmail.com>
2020-12-10 17:20:29 +0700

    panduan mobile view

--- client/src/components/home/Help.jsx
+++ client/src/components/home/Help.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 // import Prosedur_Hardware from './../../docs/Prosedur_Hardware.pdf'
 import Prosedur_Software from './../../docs/Prosedur_Software.pdf'
 import css from './help.module.css'
 import Button from '@material-ui/core/Button'
 
 function Help(props) {
-  const [isLoading, setIsLoading] = useState(false)
+  // const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     props.onView()
@@ -14,25 +14,29 @@ function Help(props) {
   return (
     <div className={css.help}>
       <p>Panduan Pembuatan Akun Baru SIAB Indonesia</p>
-      <iframe
-        title="video ab"
-        width="644"
-        height="362"
-        src="https://www.youtube.com/embed/z1i3nrJmYMM"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen="1"
-      ></iframe>
+      <div className={css.video__container}>
+        <iframe
+          title="video ab"
+          width="644"
+          height="362"
+          src="https://www.youtube.com/embed/z1i3nrJmYMM"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen="1"
+        ></iframe>
+      </div>
       <p>Panduan Penggunaan Akun Pengelola</p>
-      <iframe
-        title="video ac"
-        width="644"
-        height="362"
-        src="https://www.youtube.com/embed/zje3KjcIcJE"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen="1"
-      ></iframe>
+      <div className={css.video__container}>
+        <iframe
+          title="video ac"
+          width="644"
+          height="362"
+          src="https://www.youtube.com/embed/zje3KjcIcJE"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen="1"
+        ></iframe>
+      </div>
       <div className={css.manualbook__container}>
         <Button variant="contained" color="primary">
           <a href={Prosedur_Software}>Download Buku Panduan</a>
